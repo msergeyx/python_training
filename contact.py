342fdd870acbb2d834dcdbb97594062c9971c8a1
msergeyx <mihalev93@yandex.ru>
2015-08-24 01:43:27 +0300

    Test for contacts

---
+++ contact.py
@@ -0,0 +1,23 @@
+__author__ = 'msergeyx'
+
+
+class Contact:
+    def __init__(self, firstname, middlename, lastname, nick, tytle, company, address, home_tel, mobile_tel, work_tel,
+                 fax, birth_day, birth_month, birth_year, second_addr, second_phone, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nick = nick
+        self.tytle = tytle
+        self.company = company
+        self.address = address
+        self.home_tel = home_tel
+        self.mobile_tel = mobile_tel
+        self.work_tel = work_tel
+        self.fax = fax
+        self.birth_day = birth_day
+        self.birth_month = birth_month
+        self.birth_year = birth_year
+        self.second_addr = second_addr
+        self.second_phone = second_phone
+        self.notes = notes
