85e302f91112b79e71ec256b5d93de45402e4782
msergeyx <mihalev93@yandex.ru>
2015-09-20 15:41:28 +0300

    Added test for modifying group

--- fixture/group.py
+++ fixture/group.py
@@ -37,6 +37,27 @@ class GroupHelper:
         wd.find_element_by_name("delete").click()
         self.return_to_group_page()
 
+    def modify_first_group(self, group):
+        wd = self.app.wd
+        self.open_group_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # click edit
+        wd.find_element_by_name("edit").click()
+        # modify group
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.gr_name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.gr_header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.gr_footer)
+        # click update
+        wd.find_element_by_name("update").click()
+        self.return_to_group_page()
+
     def open_group_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,1 +1,8 @@
 __author__ = 'msergeyx'
+from model.group import Group
+
+
+def test_modify_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(gr_name="123", gr_header="345", gr_footer="456"))
+    app.session.logout()
