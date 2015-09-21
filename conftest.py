42976ed16d873d45e0ff9badd89e8ff359337163
msergeyx <mihalev93@yandex.ru>
2015-09-22 00:57:31 +0300

    Added intellectual login and logout

--- conftest.py
+++ conftest.py
@@ -11,17 +11,16 @@ def app(request):
     global fixture
     if fixture is None:
         fixture = Application()
-        fixture.session.login(username="admin", password="secret")
     elif not fixture.is_valid():
         fixture = Application()
-        fixture.session.login(username="admin", password="secret")
+    fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
     def final():
-        fixture.session.logout()
+        fixture.session.ensure_logout()
         fixture.destroy()
     request.addfinalizer(final)
     return fixture
