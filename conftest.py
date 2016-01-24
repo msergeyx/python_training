6df73c5634d9857309017fc296b42e79fa343286
msergeyx <mihalev93@yandex.ru>
2016-01-24 23:29:38 +0300

    Get info from .json file and changed path

--- conftest.py
+++ conftest.py
@@ -1,23 +1,26 @@
 __author__ = 'msergeyx'
 from fixture.application import Application
 import pytest
+import json
+import os.path
 
 
 fixture = None
+target = None
 
 
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    username = request.config.getoption("--username")
-    password = request.config.getoption("--password")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
-    elif not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username=username, password=password)
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+    if fixture is None or fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target['baseUrl'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -32,6 +35,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
-    parser.addoption("--username", action="store", default="admin")
-    parser.addoption("--password", action="store", default="secret")
+    parser.addoption("--target", action="store", default="target.json")
