ee296479f79647ac3c3d88f0ba1533192500787e
msergeyx <mihalev93@yandex.ru>
2016-01-28 23:59:33 +0300

    Changes in last commit

--- model/contact.py
+++ model/contact.py
@@ -27,7 +27,7 @@ class Contact:
         self.email = email
 
     def __repr__(self):
-        return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
+        return "%s:%s:%s:%s:%s:%s" % (self.id, self.firstname, self.lastname, self.address, self.email, self.all_phones_from_home_page)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname and self.lastname == other.lastname
--- test/test_compare_contact.py
+++ test/test_compare_contact.py
@@ -17,9 +17,22 @@ def test_compare_contact_on_home_page(app):
 
 
 def test_compare_contact_on_home_page_with_db(app, db):
-    contact_from_ui = app.contact.get_cont_list()
-    contact_from_db = db.get_cont_list()
-    assert sorted(contact_from_ui, key=Contact.id_or_max) == sorted(contact_from_db, key=Contact.id_or_max)
+    contact_from_ui = sorted(app.contact.get_cont_list(), key=Contact.id_or_max)
+    contact_from_db = sorted(db.get_cont_list(), key=Contact.id_or_max)
+    for db_contact in range(len(contact_from_db)):
+        for ui_contact in range(len(contact_from_ui)):
+            assert contact_from_ui[ui_contact].id == contact_from_db[db_contact].id
+            assert contact_from_ui[ui_contact].firstname == contact_from_db[db_contact].firstname
+            assert contact_from_ui[ui_contact].lastname == contact_from_db[db_contact].lastname
+            assert contact_from_ui[ui_contact].address == contact_from_db[db_contact].address
+            assert contact_from_ui[db_contact].all_phones_from_home_page == "\n".join(filter(lambda x: x is not None,
+                                                                                    [contact_from_db[db_contact].home_tel,
+                                                                                     contact_from_db[db_contact].mobile_tel,
+                                                                                     contact_from_db[db_contact].work_tel,
+                                                                                     contact_from_db[db_contact].second_phone]))
+        ui_contact += 1
+    db_contact += 1
+
 
 
 def clear(s):
